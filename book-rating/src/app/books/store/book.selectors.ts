4761adf1ab722d8d6431ee95750b123121c89a54
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-05-15 17:17:14 +0200

    selectors

--- book-rating/src/app/books/store/book.selectors.ts
+++ book-rating/src/app/books/store/book.selectors.ts
@@ -4,3 +4,21 @@ import * as fromBook from './book.reducer';
 export const selectBookState = createFeatureSelector<fromBook.State>(
   fromBook.bookFeatureKey
 );
+
+export const selectBooksLoading = createSelector(
+  selectBookState,
+  (state) => state.loading
+);
+
+export const selectBooks = createSelector(
+  selectBookState,
+  state => state.books
+);
+
+// just an example -- computed selector
+
+export const bookWithIsbn = createSelector(
+  selectBooks,
+  (books, props) => books.find(b => b.isbn === props.isbn)
+);
+
