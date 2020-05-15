3fd15fccd999c6c064807baa9e9a95c1fc114399
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-05-15 12:02:24 +0200

    rxjs: hands on!

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { of, Observable, timer, Subscription } from 'rxjs';
+import { of, Observable, timer, Subscription, from } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -26,7 +27,13 @@ export class BookDetailsComponent implements OnInit, OnDestroy {
     };
 
     // Observable
-    const observable = timer(0, 500);
+    const observable = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).pipe(
+      map(x => x * 10)
+      // Hand on!
+      // 2. filtere Werte heraus, so das nur noch Werte größer als 30 emittiert werden
+      // 3. bilde die Summe aus allem (nur einmal!)
+      // 4. fies: Zeige die selbe Anzahl an Raketen (🚀) an, wie die Summer groß ist
+    );
 
     // Subcription
     this.subscription = observable.subscribe(observer);
